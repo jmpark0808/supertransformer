97c8ae807faa1c77d59daff2fee1fec24c31aea0
Jinman Park <jmpark0808@gmail.com>
2022-12-07 16:23:17 -0500

    gantt chart and playing around with resampling

---
+++ Analysis/gantt.py
@@ -0,0 +1,19 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame([
+    dict(Task="Advanced topics in graphical deep learning", Start='2023-01-01', Finish='2023-04-30', Resource='Course work'),
+    dict(Task="Workshop NeurIPS", Start='2023-05-01', Finish='2023-11-15', Resource='Conference'),
+    dict(Task="Main conference CVPR", Start='2023-11-18', Finish='2024-03-28', Resource='Conference'),
+    dict(Task="Main conference NeurIPS", Start='2024-05-19', Finish='2024-09-14', Resource='Conference'),
+    dict(Task="Thesis writing", Start='2024-10-01', Finish='2025-07-01', Resource='Thesis'),
+    dict(Task="Thesis defense", Start='2025-07-01', Finish='2025-08-01', Resource='Thesis'),
+    dict(Task="Colour features", Start='2023-01-01', Finish='2023-06-01', Resource='Concept milestone'),
+    dict(Task="Shape features", Start='2023-06-01', Finish='2023-12-31', Resource='Concept milestone'),
+    dict(Task="Model work", Start='2024-01-01', Finish='2024-06-01',Resource='Concept milestone'),
+    dict(Task="Generalization to other CV tasks", Start='2024-06-01', Finish='2025-04-01',Resource='Concept milestone'),
+])
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", title='Gantt chart of future plans', color='Resource')
+fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
+fig.show()
--- Wrappers/SP_TFM.py
+++ Wrappers/SP_TFM.py
@@ -18,7 +18,7 @@ class SP_TFM_Wrapper(pl.LightningModule):
         self.es_patience = kwargs.get('es_patience')
 
         # Generator that produces the HeatMap
-        self.supert = SP_TFM(11, 16, 8, 6)
+        self.supert = SP_TFM(8+(RESAMPLE_FFT_POINTS-1)*2, 32, 8, 6)
         self.iteration = 0
         self.test_iteration = 0
         self.save_hyperparameters()
