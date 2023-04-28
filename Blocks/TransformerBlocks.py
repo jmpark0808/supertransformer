Fix softmax bug, add baseline and baseline with DPE

Blocks/TransformerBlocks.py
@@ -103,7 +103,7 @@ class PosAttention(nn.Module):
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
 
         dots = torch.matmul(q, k.transpose(-1, -2))
-        zero_vec = 0*torch.ones_like(dots)
+        zero_vec = -1e99*torch.ones_like(dots)
 
         adj = torch.matrix_power(adj, self.dilation).bool().int()
         adj = adj.unsqueeze(1).bool() # B x 1 x R x R
@@ -115,9 +115,10 @@ class PosAttention(nn.Module):
             Srel = self.skew(QEr)
             attention = torch.where(adj > 0, dots+Srel, zero_vec)
         else:
-            distances = self.distances_linear(distances)
-            QEr = torch.einsum('abcd,aced->abce', q, distances)
-            attention = torch.where(adj > 0, dots+QEr, zero_vec)
+            # distances = self.distances_linear(distances)
+            # QEr = torch.einsum('abcd,aced->abce', q, distances)
+            # attention = torch.where(adj > 0, dots+QEr, zero_vec)
+            attention = torch.where(adj > 0, dots, zero_vec)
 
         attn = self.attend((attention)*self.scale)
 

Models/SP_TFM.py
@@ -11,28 +11,18 @@ class SP_TFM(nn.Module):
     Pure Global aggregation using transformers
     Deterministic Positional Encoding 
     '''
-    def __init__(self, nfeat, nhid, nheads, ntfm, dropout):
+    def __init__(self, seq_len, nfeat, nhid, nheads, ntfm, dropout):
         """Dense version of GAT."""
         super(SP_TFM, self).__init__()
-        self.linear = nn.Linear(nfeat-2, nhid * nheads)
-        # self.pos = nn.Linear(2, nhid*nheads)
+        self.linear = nn.Linear(nfeat, nhid * nheads)
         self.encoder = nn.TransformerEncoderLayer(d_model=nhid*nheads, nhead=nheads, dropout=dropout, dim_feedforward=nhid*nheads, batch_first=True)
         self.transformer_enc = nn.TransformerEncoder(self.encoder, num_layers=ntfm)
-        # self.transformers_enc = Transformer(nhid*nheads, ntfm, nheads, nhid, nhid*nheads, dropout)
-        # self.transformers = nn.ModuleList([GraphConvTransformer(nhid*nheads, block_depth, nheads, nhid, nheads*nhid, num_regions, norm=norm, dropout=dropout) for _ in range(ntfm)])
-        # self.decoder = nn.TransformerDecoderLayer(d_model=nhid*nheads, nhead=nheads, dim_feedforward=nhid*nheads, batch_first=True)
-        # self.transformer_dec = nn.TransformerDecoder(self.decoder, num_layers=ntfm)
-
-        self.pos_encoding = PositionalEncodingSuperPixel(nhid*nheads)
+        self.pos_encoding = nn.Parameter(torch.randn(1, seq_len, nhid*nheads))
         self.out = nn.Linear(nhid * nheads, 1)
     def forward(self, x):
-        centroids = x[:, :, :2]
-        x = x[:, :, 2:]
         x = self.linear(x)
-
-        x += self.pos_encoding(centroids)
+        x += self.pos_encoding
         x = self.transformer_enc(x)
-        # x = self.transformer_dec(x, x)
         x = self.out(x)
         return x
     
@@ -41,25 +31,25 @@ class SP_TFM_REL(nn.Module):
     Pure Global aggregation using transformers
     Deterministic Positional Encoding 
     '''
-    def __init__(self, nfeat, coeff, dilation, nhid, nheads, ntfm, dropout):
+    def __init__(self, nfeat, dilation, nhid, nheads, ntfm, dropout):
         """Dense version of GAT."""
         super(SP_TFM_REL, self).__init__()
-        self.linear = nn.Linear(nfeat-2+((coeff//2*2-1)*2), nhid * nheads)
+        self.linear = nn.Linear(nfeat, nhid * nheads)
         self.pos_linear = nn.Linear(2, nhid*nheads)
 
-        self.pos_encoding = PositionalEncodingSuperPixel(nhid*nheads)
+        # self.pos_encoding = PositionalEncodingSuperPixel(nhid*nheads)
         self.transformer_enc = PosTransformer(nhid * nheads, dilation, ntfm, nheads, nhid, nhid*nheads, dropout)
         # self.encoder = nn.TransformerEncoderLayer(d_model=nhid*nheads, nhead=nheads, dropout=dropout, dim_feedforward=nhid*nheads, batch_first=True)
         # self.transformer_enc = nn.TransformerEncoder(self.encoder, num_layers=ntfm)
 
         self.out = nn.Linear(nhid * nheads, 1)
     def forward(self, x, adj, distances):
-        # pos = x[:, :, :2]
+        pos = x[:, :, :2]
         x = x[:, :, 2:]
         x = self.linear(x)
-        # pos = self.pos_linear(pos)
+        pos = self.pos_linear(pos)
         # pos = self.pos_encoding(pos)
-        # x += pos
+        x += pos
         x = self.transformer_enc(x, None, adj, distances)
 
         x = self.out(x)

dataset/superpixel.py
@@ -65,9 +65,10 @@ class RandomFlip(object):
 
 
 class ToTensorSP(object):
-    def __init__(self, num_seg):
+    def __init__(self, num_seg, compactness):
         self.tensor = transforms.ToTensor()
         self.num_seg = num_seg
+        self.compactness = compactness
 
     def __call__(self, sample):
         img, mask = sample['image'], sample['mask']
@@ -75,50 +76,37 @@ class ToTensorSP(object):
         img_size = img_np.shape[1]
         mask_np = np.array(mask)/255.
         segments = slic(img_np, n_segments=self.num_seg,
-            compactness=COMPACTNESS,
+            compactness=self.compactness,
             max_num_iter=3,
             convert2lab=True,
-            enforce_connectivity=True,
+            enforce_connectivity=False,
             slic_zero=False)
-        # slic = SlicAvx2(num_components=self.num_seg, compactness=10)
-        # segments = slic.iterate(img_np)
-
-        vs_right = np.vstack([segments[:,:-1].ravel(), segments[:,1:].ravel()])
-        vs_below = np.vstack([segments[:-1,:].ravel(), segments[1:,:].ravel()])
-        vs_diagonal_r = np.vstack([segments[:-1,:-1].ravel(), segments[1:,1:].ravel()])
-        vs_diagonal_l = np.vstack([segments[1:,:-1].ravel(), segments[:-1,1:].ravel()])
-        bneighbors = np.unique(np.hstack([vs_right, vs_below, vs_diagonal_r, vs_diagonal_l]), axis=1)
+   
     
 
-        regions = regionprops_table(segments, intensity_image=img_np, properties=('label', 'centroid', 'area', 'intensity_mean',
-                                                                                    'extent', 'coords', 'eccentricity'), extra_properties=[image_stdev])#, polarize])
+        regions = regionprops_table(segments, intensity_image=img_np, properties=('label', 'area', 'intensity_mean',
+                                                                                     'coords'), extra_properties=[image_stdev])#, polarize])
                     
         seq_len = len(regions['label'])
-        features = np.zeros([self.num_seg, 11])
+        features = np.zeros([self.num_seg, 7])
         seq_mask = np.zeros([self.num_seg])
         label = regions['label']
-        features[label-1, 0] = regions['centroid-0']
-        features[label-1, 1] = regions['centroid-1']
-        features[label-1, 2] = regions['area'] / (img_size**2)
-        features[label-1, 3] = regions['intensity_mean-0']/255.
-        features[label-1, 4] = regions['intensity_mean-1']/255.
-        features[label-1, 5] = regions['intensity_mean-2']/255.
-        features[label-1, 6] = regions['extent']
-        features[label-1, 7] = regions['eccentricity']
-        features[label-1, 8] = regions['image_stdev-0']/255.
-        features[label-1, 9] = regions['image_stdev-1']/255.
-        features[label-1, 10] = regions['image_stdev-2']/255.
-        # for i in range(NUM_CHUNK*2):
-        #     features[label-1, 11+i] = regions[f'polarize-{i}-0']
-        #     features[label-1, 11+NUM_CHUNK*2+i] = regions[f'polarize-{i}-1']
+        features[label-1, 0] = regions['area'] / (img_size**2)
+        features[label-1, 1] = regions['intensity_mean-0']/255.
+        features[label-1, 2] = regions['intensity_mean-1']/255.
+        features[label-1, 3] = regions['intensity_mean-2']/255.
+        features[label-1, 4] = regions['image_stdev-0']/255.
+        features[label-1, 5] = regions['image_stdev-1']/255.
+        features[label-1, 6] = regions['image_stdev-2']/255.
+
 
         for ind, coord in zip(regions['label'], regions['coords']):
             seq_mask[ind-1] = 1 if np.sum(mask_np[coord[:, 0], coord[:, 1]])/len(coord[:, 0]) >= 0.5 else 0
 
         neighbor_array = np.zeros([self.num_seg, self.num_seg])
         # eye = np.eye(self.num_seg)
-        neighbor_array[bneighbors[0]-1, bneighbors[1]-1] = 1
-        neighbor_array[bneighbors[1]-1, bneighbors[0]-1] = 1
+        # neighbor_array[bneighbors[0]-1, bneighbors[1]-1] = 1
+        # neighbor_array[bneighbors[1]-1, bneighbors[0]-1] = 1
         # neighbor_array -= eye
 
 

util/util.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import numpy as np
-
+from dataset.constants import *
 
 def estimate_memory_training(model, sample_input, optimizer_type=torch.optim.Adam, batch_size=1, use_amp=False, device=0):
     """Predict the maximum memory usage of the model. 
@@ -146,3 +146,12 @@ def resample_2d(points, N):
     xi = np.interp(dSi, d, xc)
     yi = np.interp(dSi, d, yc)
     return xi, yi
+
+
+def get_input_dim(dataloader, args):
+    if dataloader == 'SP':
+        return 7 
+    elif dataloader == 'SPFFT':
+        return 6+((args.get('coeff')//2*2-1)*2)
+    else:
+        raise 'Unrecognized dataloader'