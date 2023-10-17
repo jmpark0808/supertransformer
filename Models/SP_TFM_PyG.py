38c766ece81ef2654dafb2c12c0f7b10fa3dce81
Jinman Park <jmpark0808@gmail.com>
2023-10-17 14:01:57 -0400

    Comparison between pyg and pyt and then fix TFM PyG to be similar

--- Models/SP_TFM_PyG.py
+++ Models/SP_TFM_PyG.py
@@ -20,10 +20,10 @@ class SP_TFM_PyG(nn.Module):
         self.pos_linear = nn.Linear(2, nhid*nheads)
         self.convs = nn.ModuleList([TransformerConv(in_channels=nhid*nheads, out_channels=nhid,
                                                                           heads=nheads, dropout=dropout, edge_dim=2,
-                                                                            concat=True) for _ in range(ntfm)])
-        # self.ffs = nn.ModuleList([FeedForward(nhid*nheads, nhid*nheads, dropout) for _ in range(ntfm)])
+                                                                            concat=True, root_weight=False) for _ in range(ntfm)])
+        self.ffs = nn.ModuleList([FeedForward(nhid*nheads, nhid*nheads, dropout) for _ in range(ntfm)])
         self.ln1s = nn.ModuleList([LayerNorm(nhid*nheads) for _ in range(ntfm)])
-        # self.ln2s = nn.ModuleList([LayerNorm(nhid*nheads) for _ in range(ntfm)])
+        self.ln2s = nn.ModuleList([LayerNorm(nhid*nheads) for _ in range(ntfm)])
         self.classifier = nn.Linear(nhid*nheads, 1)
         self.num_seg = num_seg
         
@@ -35,25 +35,21 @@ class SP_TFM_PyG(nn.Module):
 
         batch_size = x.size(0)//self.num_seg
         batch_index = torch.arange(0, batch_size).repeat(self.num_seg).reshape(self.num_seg, -1).T.reshape(-1).cuda()
-        # pos = x[:, :2]
+        pos = x[:, :2]
         x = x[:, 2:]
 
         x = self.linear1(x)
-        x = self.elu(x)
 
-        # pos = self.pos_linear(pos)
-        # x += pos
+        pos = self.pos_linear(pos)
+        x += pos
 
-        # for conv, ff, ln1, ln2 in zip(self.convs, self.ffs, self.ln1s, self.ln2s):
+        for conv, ff, ln1, ln2 in zip(self.convs, self.ffs, self.ln1s, self.ln2s):
+            x = conv(ln1(x, batch_index), edge_index=edge_index, edge_attr=None) + x# adding edge features here
+            x = ff(ln2(x, batch_index)) + x
+        # for conv, ln1 in zip(self.convs, self.ln1s):
         #     x = ln1(x, batch_index)
         #     x = conv(x, edge_index=edge_index, edge_attr=None)# adding edge features here
-        #     x = ln2(x, batch_index)
-        #     x = ff(x) + x
-        for conv, ln1 in zip(self.convs, self.ln1s):
-            x = ln1(x, batch_index)
-            x = conv(x, edge_index=edge_index, edge_attr=edge_attr)# adding edge features here
-            x = self.elu(x)
+        #     x = self.elu(x)
       
-        # x = self.convs[-1](x, edge_index, edge_attr=edge_attr)
         x = self.classifier(x)
         return x
