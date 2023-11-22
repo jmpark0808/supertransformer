44d2a67e3bba68a6f4e60c5006e3e75b93a92d6d
Jinman <jmpark0808@gmail.com>
2023-11-22 14:12:31 -0500

    Script for generating imagenet data

--- dataset/imagenet.py
+++ dataset/imagenet.py
@@ -269,6 +269,231 @@ class ImageNetDatasetTrain(torchvision.datasets.ImageFolder):
 
             return features, target, torch.tensor(neighbor_array)
 
+
+class ImageNetDatasetTestExport(data.Dataset):
+    def __init__(self, root_dir, transforms, num_seg, coeff, class_to_idx, compactness, export_dir):
+        self.root_dir = root_dir
+        self.image_list = sorted(os.listdir('{}/Data/CLS-LOC/val'.format(root_dir)))
+        self.target_list = sorted(os.listdir('{}/Annotations/CLS-LOC/val'.format(root_dir)))
+        self.transform = transforms
+        self.class_to_idx = class_to_idx
+        self.num_seg = num_seg
+        self.compactness = compactness
+        self.coeff = coeff
+        self.export_dir = export_dir
+
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, item):
+        img_name = '{}/Data/CLS-LOC/val/{}'.format(self.root_dir, self.image_list[item])
+        target_name = '{}/Annotations/CLS-LOC/val/{}'.format(self.root_dir, self.target_list[item])
+
+        def fourier_descriptors(region):
+            region = (region*255).astype(np.uint8)
+            contour, hierarchy = cv2.findContours(region, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            points = contour[0][:, 0, :]
+            xi, yi = resample_2d(points, RESAMPLE_POINTS)
+            contour_array = np.stack((xi, yi), axis=1)
+
+
+            contour_complex = np.empty(contour_array.shape[:-1], dtype=complex)
+            contour_complex.real = contour_array[:, 0]
+            contour_complex.imag = contour_array[:, 1]
+            fourier_result = np.fft.fft(contour_complex)
+
+            fourier_result_front = fourier_result[1:1+self.coeff//2]
+            fourier_result_back = fourier_result[-self.coeff//2:]
+            fourier_result = np.concatenate((fourier_result_front, fourier_result_back), axis=0)
+
+            amp = abs(fourier_result)
+            phase = np.arctan2(fourier_result.imag, fourier_result.real)
+
+            # return np.array(amp)
+            return np.concatenate((amp, phase))
+
+        sp_file_name = self.image_list[item].split('.')[0]+'.npy'
+        sp_file_name_edge = self.image_list[item].split('.')[0]+'edge.npy'
+   
+        sp_file_path = os.path.join(self.export_dir, sp_file_name)
+        sp_file_path_edge_index = os.path.join(self.export_dir, sp_file_name_edge)
+
+        target = ET.parse(target_name)
+        root = target.getroot()
+        target = root[5][0].text
+        target = self.class_to_idx[target]
+
+        
+        img = Image.open(img_name)
+        img = img.convert('RGB')
+        img = self.transform(img)
+
+        img_np = np.ascontiguousarray(np.transpose(img.cpu().numpy()*255, (1, 2, 0))).astype(np.uint8)
+
+        # segments = slic(img_np, n_segments=self.num_seg,
+        #     compactness=self.compactness,
+        #     max_num_iter=3,
+        #     convert2lab=True,
+        #     enforce_connectivity=False,
+        #     slic_zero=False)
+
+        slic = SlicAvx2(num_components=self.num_seg, compactness=self.compactness)
+        segments = slic.iterate(img_np)+1
+
+        vs_right = np.vstack([segments[:,:-1].ravel(), segments[:,1:].ravel()])
+        vs_below = np.vstack([segments[:-1,:].ravel(), segments[1:,:].ravel()])
+        vs_diagonal_r = np.vstack([segments[:-1,:-1].ravel(), segments[1:,1:].ravel()])
+        vs_diagonal_l = np.vstack([segments[1:,:-1].ravel(), segments[:-1,1:].ravel()])
+        bneighbors = np.unique(np.hstack([vs_right, vs_below, vs_diagonal_r, vs_diagonal_l]), axis=1)
+        neighbor_array = np.zeros([self.num_seg, self.num_seg])
+        neighbor_array[bneighbors[0]-1, bneighbors[1]-1] = 1
+        neighbor_array[bneighbors[1]-1, bneighbors[0]-1] = 1
+
+        np.save(sp_file_path_edge_index, neighbor_array)
+        regions = regionprops_table(segments, intensity_image=img_np, properties=('label', 'centroid', 'intensity_mean',
+                                                                                    'coords'), extra_properties=[image_stdev, fourier_descriptors])#, polarize])
+
+        seq_len = len(regions['label'])
+        label = regions['label']
+        features = np.zeros([self.num_seg, 8+(self.coeff)*2])
+        
+        for i in range(self.coeff*2):
+            features[label-1, 8+i] = regions[f'fourier_descriptors-{i}']
+
+        
+        features[label-1, 0] = regions['centroid-0']
+        features[label-1, 1] = regions['centroid-1']
+        features[label-1, 2] = regions['intensity_mean-0']/255.
+        features[label-1, 3] = regions['intensity_mean-1']/255.
+        features[label-1, 4] = regions['intensity_mean-2']/255.
+        features[label-1, 5] = regions['image_stdev-0']/255.
+        features[label-1, 6] = regions['image_stdev-1']/255.
+        features[label-1, 7] = regions['image_stdev-2']/255.
+
+        np.save(sp_file_path, features)
+
+        features, target = torch.tensor(features).float(), torch.tensor(target)
+
+        return features, target, torch.tensor(neighbor_array)
+
+
+
+class ImageNetDatasetTrainExport(torchvision.datasets.ImageFolder):
+    def __init__(self, root, num_seg, coeff, compactness, transform, mode, export_dir) -> None:
+        super().__init__(root, transform=transform)
+        self.num_seg = num_seg
+        self.compactness = compactness
+        self.coeff = coeff
+        self.mode = mode
+        self.export_dir = export_dir
+        
+
+   
+
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+
+        img, target = self.imgs[index], self.targets[index]
+        def fourier_descriptors(region):
+            region = (region*255).astype(np.uint8)
+            contour, hierarchy = cv2.findContours(region, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            points = contour[0][:, 0, :]
+            xi, yi = resample_2d(points, RESAMPLE_POINTS)
+            contour_array = np.stack((xi, yi), axis=1)
+
+
+            contour_complex = np.empty(contour_array.shape[:-1], dtype=complex)
+            contour_complex.real = contour_array[:, 0]
+            contour_complex.imag = contour_array[:, 1]
+            fourier_result = np.fft.fft(contour_complex)
+
+            fourier_result_front = fourier_result[1:1+self.coeff//2]
+            fourier_result_back = fourier_result[-self.coeff//2:]
+            fourier_result = np.concatenate((fourier_result_front, fourier_result_back), axis=0)
+
+            amp = abs(fourier_result)
+            phase = np.arctan2(fourier_result.imag, fourier_result.real)
+
+            # return np.array(amp)
+            return np.concatenate((amp, phase))
+        sp_file_name = pathlib.PureWindowsPath(rf'{img[0]}').as_posix().split('/')[-1].split('.')[0]+'.npy'
+        sp_file_name_edge = pathlib.PureWindowsPath(rf'{img[0]}').as_posix().split('/')[-1].split('.')[0]+'edge.npy'
+
+        sp_file_path = os.path.join(self.export_dir, sp_file_name)
+        sp_file_path_edge_index = os.path.join(self.export_dir, sp_file_name_edge)
+
+
+        
+        img = Image.open(img[0])
+        img = img.convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+
+        img_np = np.ascontiguousarray(np.transpose(img.cpu().numpy()*255, (1, 2, 0))).astype(np.uint8)
+        
+
+        # segments = slic(img_np, n_segments=self.num_seg,
+        #     compactness=self.compactness,
+        #     max_num_iter=3,
+        #     convert2lab=True,
+        #     enforce_connectivity=False,
+        #     slic_zero=False,
+        #     min_size_factor=0)
+        slic = SlicAvx2(num_components=self.num_seg, compactness=self.compactness)
+        segments = slic.iterate(img_np)+1
+        # plt.imshow(mark_boundaries(img_np, segments))
+        # plt.show()
+        vs_right = np.vstack([segments[:,:-1].ravel(), segments[:,1:].ravel()])
+        vs_below = np.vstack([segments[:-1,:].ravel(), segments[1:,:].ravel()])
+        vs_diagonal_r = np.vstack([segments[:-1,:-1].ravel(), segments[1:,1:].ravel()])
+        vs_diagonal_l = np.vstack([segments[1:,:-1].ravel(), segments[:-1,1:].ravel()])
+        bneighbors = np.unique(np.hstack([vs_right, vs_below, vs_diagonal_r, vs_diagonal_l]), axis=1)
+        neighbor_array = np.zeros([self.num_seg, self.num_seg])
+        neighbor_array[bneighbors[0]-1, bneighbors[1]-1] = 1
+        neighbor_array[bneighbors[1]-1, bneighbors[0]-1] = 1
+
+        np.save(sp_file_path_edge_index, neighbor_array)
+        regions = regionprops_table(segments, intensity_image=img_np, properties=('label', 'centroid', 'intensity_mean',
+                                                                                    'coords'), extra_properties=[image_stdev, fourier_descriptors])#, polarize])
+
+        seq_len = len(regions['label'])
+        label = regions['label']
+        features = np.zeros([self.num_seg, 8+(self.coeff)*2])
+    
+        for i in range(self.coeff*2):
+            features[label-1, 8+i] = regions[f'fourier_descriptors-{i}']
+
+        
+        features[label-1, 0] = regions['centroid-0']
+        features[label-1, 1] = regions['centroid-1']
+        features[label-1, 2] = regions['intensity_mean-0']/255.
+        features[label-1, 3] = regions['intensity_mean-1']/255.
+        features[label-1, 4] = regions['intensity_mean-2']/255.
+        features[label-1, 5] = regions['image_stdev-0']/255.
+        features[label-1, 6] = regions['image_stdev-1']/255.
+        features[label-1, 7] = regions['image_stdev-2']/255.
+
+        
+        np.save(sp_file_path, features)
+
+        features, target = torch.tensor(features).float(), torch.tensor(target)
+
+        return features, target, torch.tensor(neighbor_array)
+
+
+
         
 
 class SPImageNetDataModule(pl.LightningDataModule):
@@ -298,12 +523,14 @@ class SPImageNetDataModule(pl.LightningDataModule):
         self.num_seg = kwargs.get('num_seg', 600)
         self.coeff = kwargs.get('coeff', 70)
         self.compactness = kwargs.get('compactness', 10)
+        self.seed = kwargs.get('seed')
+        generator = torch.Generator().manual_seed(self.seed)
 
         train_dataset = ImageNetDatasetTrain(train_dir, self.num_seg, self.coeff, self.compactness, val_test_transform, 'train')
         class_to_idx = train_dataset.class_to_idx
         train_size = int(0.8*len(train_dataset))
         val_size = len(train_dataset) - train_size
-        train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size])
+        train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size], generator=generator)
         val_dataset.dataset.transform = val_test_transform
         val_dataset.mode = 'val'
 
---
+++ util/imagenet_data_prep.py
@@ -0,0 +1,69 @@
+from dataset.imagenet import ImageNetDatasetTrainExport, ImageNetDatasetTestExport
+import torch
+import torchvision.transforms as transforms
+import argparse
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--train_dir', help='Directory of your ImageNet Train Dataset', required=True, default=None)
+    parser.add_argument('--test_dir', help='Directory of your ImageNet Test Dataset', required=True, default=None)
+    parser.add_argument('--train_export_dir', help='Directory of the train saved graphs', required=True, default=None)
+    parser.add_argument('--test_export_dir', help='Directory of the test saved graphs', required=True, default=None)
+    parser.add_argument('--batch_size', help="batchsize, default = 1", default=1, type=int)
+    parser.add_argument('--num_workers', help="# of dataloader cpu process", default=0, type=int)
+  
+
+
+
+    args = parser.parse_args()
+    dict_args = vars(args)
+
+    seed = 22
+    generator = torch.Generator().manual_seed(seed)
+    train_dir = dict_args['train_dir']
+    test_dir = dict_args['test_dir']
+    num_seg = 625
+    coeff = 10
+    compactness = 10
+    batch_size = dict_args['batch_size']
+    num_workers = dict_args['num_workers']
+    train_export_dir = dict_args['train_export_dir']
+    test_export_dir = dict_args['test_export_dir']
+
+    val_test_transform = transforms.Compose(
+                            [transforms.Resize([256, 256]),
+                            transforms.ToTensor()
+                            ])
+
+    train_dataset = ImageNetDatasetTrainExport(train_dir, num_seg, coeff, compactness, val_test_transform, 'train', train_export_dir)
+    class_to_idx = train_dataset.class_to_idx
+    train_size = int(0.8*len(train_dataset))
+    val_size = len(train_dataset) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size], generator=generator)
+    val_dataset.dataset.transform = val_test_transform
+    val_dataset.mode = 'val'
+
+    test_dataset = ImageNetDatasetTestExport(test_dir, val_test_transform, num_seg, coeff, class_to_idx, compactness, test_export_dir)
+
+    train_source_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                                                            num_workers =num_workers, drop_last=False)
+
+    val_source_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
+                                                            num_workers=num_workers, drop_last=False)
+
+    test_source_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                                                            num_workers=num_workers, drop_last=False)
+    
+    for _ in tqdm(train_source_loader):
+        pass
+
+    for _ in tqdm(val_source_loader):
+        pass
+
+    for _ in tqdm(test_source_loader):
+        pass
+
+
+
+
