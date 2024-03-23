46fc8e9610023cd87f2a58904f148f55599416f7
Jinman <jmpark0808@gmail.com>
2024-03-23 17:04:56 -0400

    Revert old ways bug fix

--- Models/SP_SWIN.py
+++ Models/SP_SWIN.py
@@ -62,7 +62,7 @@ class SP_SWIN(nn.Module):
         'head_dim': head_dim,
         'depths': None, #(tuple(int)): Depth of each Swin Transformer layer.
         'num_heads': nheads, #(tuple(int)): Number of attention heads in different layers.
-        'kernels': [8, 16, 32],
+        'kernels': [32, 32, 32, 32],
         'window_size': 8, #(int): Window size. Default: 8
         'mlp_ratio': 2.,#(float): Ratio of mlp hidden dim to embedding dim. Default: 4
         'qkv_bias': True,#(bool): If True, add a learnable bias to query, key, value. Default: True
@@ -146,7 +146,7 @@ class SP_SWIN_ImageNet(nn.Module):
         'head_dim': head_dim,
         'depths': [2]*ntfm, #(tuple(int)): Depth of each Swin Transformer layer.
         'num_heads': nheads, #(tuple(int)): Number of attention heads in different layers.
-        'kernels': [32, 32, 32, 32],
+        'kernels': [4, 8, 16, 32],
         'window_size': 4, #(int): Window size. Default: 8
         'mlp_ratio': 2.,#(float): Ratio of mlp hidden dim to embedding dim. Default: 4
         'qkv_bias': True,#(bool): If True, add a learnable bias to query, key, value. Default: True
