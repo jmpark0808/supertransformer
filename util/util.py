8a9d2d4a62a4ddb18707d2d4cae5bfc2f5941ef9
Jinman <jmpark0808@gmail.com>
2024-05-20 16:58:10 -0400

    Try ImageNet with small standard GATv2 and relative positional encoding

--- util/util.py
+++ util/util.py
@@ -155,7 +155,8 @@ def get_input_dim(args):
     elif d == 'SPF':
         return 13
     elif d == 'SPFFFT' or d== 'SPGFFT' or d == 'ImageNet' or \
-        d == 'YD' or d == 'SPGSWIN' or d == 'YDG':
+        d == 'YD' or d == 'SPGSWIN' or d == 'YDG' \
+            or d == 'ImageNet_SWIN':
         if args.get('ignore_phase'):
             return 6+(args.get('coeff'))+10
         else:
