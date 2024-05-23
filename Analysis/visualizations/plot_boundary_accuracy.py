10bc7f261d7c9204c3a5c981fe7089ce4ea21f99
Jinman <jmpark0808@gmail.com>
2024-05-23 12:47:14 -0400

    Remove torchsummary

--- Analysis/visualizations/plot_boundary_accuracy.py
+++ Analysis/visualizations/plot_boundary_accuracy.py
@@ -1,10 +1,11 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open('/home/eddie/waterloo/supertransformer/sandbox/segments_plot_data.pkl', 'rb') as f:
+with open('/home/eddie/waterloo/supertransformer/Analysis/segments_plot_data.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
 plt.figure(figsize=(10,10))
 segment_numbers = loaded_dict['segment_numbers']
+print(segment_numbers)
 compactness = [0.1, 1, 10, 50]
 for compact in compactness:
     all_ious = loaded_dict[compact]
@@ -27,5 +28,6 @@ plt.yticks(fontsize=fs)
 plt.legend(loc="lower right", fontsize=fs, title='Compactness', title_fontsize=fs)
 plt.vlines(x=segment_numbers, ymin=0.93, ymax=1, ls=":")
 plt.tight_layout()
-plt.savefig(f'compactness.jpg', bbox_inches='tight')
+# plt.savefig(f'compactness.jpg', bbox_inches='tight')
+plt.show()
     
