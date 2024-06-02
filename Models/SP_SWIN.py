897646a98411b3666e0561628e5f010cce724f71
Jinman <jmpark0808@gmail.com>
2024-06-02 09:29:04 -0400

    Try the old SWINU

--- Models/SP_SWIN.py
+++ Models/SP_SWIN.py
@@ -16,8 +16,8 @@ class SP_SWINU(nn.Module):
         self.pos_linear = nn.Linear(2, nhid)
         options = {'swin_hp': {'patch_size': 1,  # (int | tuple(int)): Patch size. Default: 4
         'embed_dim': nhid, #(int): Patch embedding dimension. Default: 96
-        'depths': [2, 2, 6, 2], #(tuple(int)): Depth of each Swin Transformer layer.
-        'num_heads': [nheads, nheads, nheads, nheads], #(tuple(int)): Number of attention heads in different layers.
+        'depths': [ntfm, ntfm, ntfm*3, ntfm], #(tuple(int)): Depth of each Swin Transformer layer.
+        'num_heads': [nheads, nheads*2, nheads*4, nheads*8], #(tuple(int)): Number of attention heads in different layers.
         'window_size': 4, #(int): Window size. Default: 8
         'mlp_ratio': 2.,#(float): Ratio of mlp hidden dim to embedding dim. Default: 4
         'qkv_bias': True,#(bool): If True, add a learnable bias to query, key, value. Default: True
@@ -35,6 +35,7 @@ class SP_SWINU(nn.Module):
         self.model = SwinUTransformer(options = options)
         self.out = nn.Linear(128+64+32+16, 1)
     def forward(self, x):
+        x = x.reshape(x.size(0), x.size(1), -1).permute(0, 2, 1)
         pos = x[:, :, :2]
         x = x[:, :, 2:]
        
