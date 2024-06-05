d36ba51d32d9f0cda0226873366845ddb6a39301
Jinman <jmpark0808@gmail.com>
2024-06-05 11:25:18 -0400

    Change Unet architecture to support imgnet backbone

--- Analysis/visualizations/superpixel_boundary.py
+++ Analysis/visualizations/superpixel_boundary.py
@@ -117,47 +117,47 @@ else:
         for i, j in zip(np.power(np.array(segment_numbers), 2), all_ious):
             plt.text(i, j+0.002, '{}'.format(i))
 
-    all_ious = []
-    for resolution in segment_numbers:
-        IoUs = []
-        for file in tqdm(os.listdir(dataset_images)[:num_images]):
-            name = file.split('.jpg')[0]
-            image = os.path.join(dataset_images, name+'.jpg')
-            mask = os.path.join(masks, name+'.png')
+    # all_ious = []
+    # for resolution in segment_numbers:
+    #     IoUs = []
+    #     for file in tqdm(os.listdir(dataset_images)[:num_images]):
+    #         name = file.split('.jpg')[0]
+    #         image = os.path.join(dataset_images, name+'.jpg')
+    #         mask = os.path.join(masks, name+'.png')
 
-            msk = Image.open(mask)
-            msk_resize = Image.open(mask)
-            msk = msk.convert('L').resize((300, 300))
-            msk_resize = msk_resize.convert('L').resize((resolution, resolution))
+    #         msk = Image.open(mask)
+    #         msk_resize = Image.open(mask)
+    #         msk = msk.convert('L').resize((300, 300))
+    #         msk_resize = msk_resize.convert('L').resize((resolution, resolution))
 
-            msk = np.array(msk)
-            msk_resize = np.array(msk_resize)
+    #         msk = np.array(msk)
+    #         msk_resize = np.array(msk_resize)
             
 
-            msk[msk<=125] = 0
-            msk[msk>125] = 1
+    #         msk[msk<=125] = 0
+    #         msk[msk>125] = 1
 
-            msk_resize[msk_resize<=125] = 0
-            msk_resize[msk_resize>125] = 1
+    #         msk_resize[msk_resize<=125] = 0
+    #         msk_resize[msk_resize>125] = 1
                     
-            plt_image = cv2.resize(msk_resize, (300, 300))
-            plt_image = np.ravel(plt_image)
-            msk = np.ravel(msk)
-            y_temp = (plt_image >= 0.5).astype(np.float)
-            tp = np.sum((y_temp * msk))
-            # avoid prec becomes 0
-            prec, recall = (tp + 1e-10) / (np.sum(y_temp) + 1e-10), (tp + 1e-10) / (np.sum(msk) + 1e-10)
-            beta_square = 0.3
-            f_score = (1 + beta_square) * prec * recall / (beta_square * prec + recall)
-            IoUs.append(f_score)
+    #         plt_image = cv2.resize(msk_resize, (300, 300))
+    #         plt_image = np.ravel(plt_image)
+    #         msk = np.ravel(msk)
+    #         y_temp = (plt_image >= 0.5).astype(np.float)
+    #         tp = np.sum((y_temp * msk))
+    #         # avoid prec becomes 0
+    #         prec, recall = (tp + 1e-10) / (np.sum(y_temp) + 1e-10), (tp + 1e-10) / (np.sum(msk) + 1e-10)
+    #         beta_square = 0.3
+    #         f_score = (1 + beta_square) * prec * recall / (beta_square * prec + recall)
+    #         IoUs.append(f_score)
 
-        all_ious.append(np.mean(IoUs))
+    #     all_ious.append(np.mean(IoUs))
 
-    d['ious'] = all_ious
-    plt.plot(np.power(np.array(segment_numbers), 2), all_ious, label='Resize')
-    plt.scatter(np.power(np.array(segment_numbers), 2), all_ious)
-    for i, j in zip(np.power(np.array(segment_numbers), 2), all_ious):
-        plt.text(i, j+0.002, '{}'.format(i))
+    # d['ious'] = all_ious
+    # plt.plot(np.power(np.array(segment_numbers), 2), all_ious, label='Resize')
+    # plt.scatter(np.power(np.array(segment_numbers), 2), all_ious)
+    # for i, j in zip(np.power(np.array(segment_numbers), 2), all_ious):
+    #     plt.text(i, j+0.002, '{}'.format(i))
 
 with open('segments_plot_data.pkl', 'wb') as f:
     pickle.dump(d, f)
