1d0f51cefaf04cc0834618d325a8543afe97ff69
Jinman <jmpark0808@gmail.com>
2024-06-17 14:49:36 -0400

    Try MBUNET

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ from Wrappers.SP_SWINU import SP_SWINU_Wrapper
 from Wrappers.SP_SWIN_Kernel import SP_SWIN_Kernel_Wrapper
 from Wrappers.SP_SWIN_PyG import SP_SWIN_PyG_Wrapper
 from Wrappers.Image_SOD_SWINU import Image_SWINU_Wrapper
+from Wrappers.SP_MBUNET import SP_MBUNET_Wrapper
 # from Wrappers.SP_MAMBA import SP_MAMBA_Wrapper
 
 
@@ -107,6 +108,7 @@ MODEL_DIRECTORY = {
     'SP_SWIN_Kernel': SP_SWIN_Kernel_Wrapper,
     'SP_SWIN_PyG': SP_SWIN_PyG_Wrapper,
     'IM_SWINU': Image_SWINU_Wrapper,
+    'SP_MBUNET': SP_MBUNET_Wrapper,
 }
 DATALOADER_DIRECTORY = {
     'SP': SPDataModule,
