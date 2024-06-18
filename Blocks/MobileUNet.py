a3b0de7ebcf27622c2a630d5a25cb2828f82ab01
Jinman <jmpark0808@gmail.com>
2024-06-17 22:17:42 -0400

    MBNET TUBE

--- Blocks/MobileUNet.py
+++ Blocks/MobileUNet.py
@@ -125,6 +125,73 @@ class MobileNetV2(nn.Module):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
+
+class MobileNetV2Tube(nn.Module):
+    def __init__(self, n_class=1000, input_size=224, width_mult=1.):
+        super(MobileNetV2Tube, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 320
+        interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 1],
+            [6, 32, 3, 1],
+            [6, 64, 4, 1],
+            [6, 96, 3, 1],
+            [6, 160, 3, 1],
+            [6, 320, 1, 1],
+        ]
+
+        # building first layer
+        assert input_size % 32 == 0
+        input_channel = int(input_channel * width_mult)
+        self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        self.features = [conv_bn(3, input_channel, 1)]
+        # building inverted residual blocks
+        for t, c, n, s in interverted_residual_setting:
+            output_channel = int(c * width_mult)
+            for i in range(n):
+                if i == 0:
+                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                else:
+                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                input_channel = output_channel
+        # building last several layers
+        self.features.append(nn.Conv2d(input_channel, 1, 1, 1))
+        # self.features.append(conv_1x1_bn(input_channel, 1))
+        # make it nn.Sequential
+        self.features = nn.Sequential(*self.features)
+
+        # building classifier
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(0.2),
+        #     nn.Linear(self.last_channel, n_class),
+        # )
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        # x = x.mean(3).mean(2)
+        # x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
 class MobileNetV2_unet(nn.Module):
     def __init__(self, input_size, pre_trained='weights/mobilenet_v2.pth.tar'):
         super(MobileNetV2_unet, self).__init__()
--- train.py
+++ train.py
@@ -44,6 +44,7 @@ from Wrappers.SP_SWIN_Kernel import SP_SWIN_Kernel_Wrapper
 from Wrappers.SP_SWIN_PyG import SP_SWIN_PyG_Wrapper
 from Wrappers.Image_SOD_SWINU import Image_SWINU_Wrapper
 from Wrappers.SP_MBUNET import SP_MBUNET_Wrapper
+from Wrappers.SP_MBNET import SP_MBNET_Wrapper
 # from Wrappers.SP_MAMBA import SP_MAMBA_Wrapper
 
 
@@ -109,6 +110,7 @@ MODEL_DIRECTORY = {
     'SP_SWIN_PyG': SP_SWIN_PyG_Wrapper,
     'IM_SWINU': Image_SWINU_Wrapper,
     'SP_MBUNET': SP_MBUNET_Wrapper,
+    'SP_MBNET': SP_MBNET_Wrapper,
 }
 DATALOADER_DIRECTORY = {
     'SP': SPDataModule,
