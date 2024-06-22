4374653e7377271d89db32aa37384f739eb20119
Jinman <jmpark0808@gmail.com>
2024-06-22 11:26:25 -0400

    Baseline for SWINU with Upernet

--- Wrappers/SP_SWINU.py
+++ Wrappers/SP_SWINU.py
@@ -4,7 +4,8 @@ from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch
 # from Blocks.swintransformer_original_rpe import SwinUTransformer
 # from Blocks.swintransformer_original import SwinUTransformer
-from Blocks.swinunet_rpe import SwinUTransformer
+# from Blocks.swinunet_rpe import SwinUTransformer
+from Blocks.swin_experimental import SwinUTransformer
 # from Models.SP_SWIN import SP_SWINU
 import torch.nn.functional as F
 import numpy as np
@@ -39,8 +40,8 @@ class SP_SWINU_Wrapper(pl.LightningModule):
         self.supert = SwinUTransformer(in_chans=16, img_size=(res, res), patch_size=1, window_size=self.window_size,
                                        depths=[self.tfm_hp[1], self.tfm_hp[1], self.tfm_hp[1]*3]
                                        , num_heads=[self.tfm_hp[0],
-                                                     self.tfm_hp[0]*2,
-                                                     self.tfm_hp[0]*4],
+                                                     self.tfm_hp[0],
+                                                     self.tfm_hp[0]],
                                        embed_dim=self.tfm_hp[2])
         # self.supert = SwinUTransformer(img_size=res, in_chans=input_dim, patch_size=1, window_size=self.window_size,
         #                                , depths=[self.tfm_hp[1], self.tfm_hp[1], self.tfm_hp[1]*3, self.tfm_hp[1]],
