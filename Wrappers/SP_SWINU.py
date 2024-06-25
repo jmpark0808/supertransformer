f9fe2fb9cbe058bc661e1dfd24da736c6133bb0c
Jinman <jmpark0808@gmail.com>
2024-06-25 16:08:37 -0400

    Add back mixup, and use patch merge

--- Wrappers/SP_SWINU.py
+++ Wrappers/SP_SWINU.py
@@ -152,10 +152,10 @@ class SP_SWINU_Wrapper(pl.LightningModule):
 
         res = int(self.num_seg**0.5)
         features = features.reshape(features.size(0), res, res, -1).permute(0, 3, 1, 2)
-        # seq_mask = seq_mask.reshape(seq_mask.size(0), res, res)
-        # features, seq_mask = self.mixup(features, seq_mask)
+        seq_mask = seq_mask.reshape(seq_mask.size(0), res, res)
+        features, seq_mask = self.mixup(features, seq_mask)
         
-        # seq_mask = seq_mask.reshape(seq_mask.size(0), -1)
+        seq_mask = seq_mask.reshape(seq_mask.size(0), -1)
 
         # forward pass
         
