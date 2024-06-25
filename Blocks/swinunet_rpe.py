4279d25d0b48df32117fcd45266ed2fd30642975
Jinman <jmpark0808@gmail.com>
2024-06-25 11:53:09 -0400

    Go back to patch attentiong

--- Blocks/swinunet_rpe.py
+++ Blocks/swinunet_rpe.py
@@ -672,7 +672,7 @@ class BasicLayerDownsample(nn.Module):
 
         # patch merging layer
         if downsample is not None:
-            self.downsample = downsample(input_resolution, dim=dim, norm_layer=norm_layer)#num_heads=num_heads)#
+            self.downsample = downsample(input_resolution, dim=dim,  num_heads=num_heads)#, norm_layer=norm_layer)
         else:
             self.downsample = None
 
@@ -903,7 +903,7 @@ class SwinUTransformer(nn.Module):
                                drop=drop_rate, attn_drop=attn_drop_rate,
                                drop_path=0,
                                norm_layer=norm_layer,
-                               downsample=PatchMerging if (i_layer < self.num_layers - 1) else None,
+                               downsample=PatchAttention if (i_layer < self.num_layers - 1) else None,
                                use_checkpoint=use_checkpoint,
                                fused_window_process=fused_window_process)
             self.layers.append(layer)
