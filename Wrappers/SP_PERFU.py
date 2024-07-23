03e2a192838027b857fb2f4bc2fe43509db4a92d
Jinman <jmpark0808@gmail.com>
2024-07-23 12:16:57 -0400

    Remove decoder for perfu for now, use relu kernel instead of softmax kernel

--- Wrappers/SP_PERFU.py
+++ Wrappers/SP_PERFU.py
@@ -65,6 +65,9 @@ class SP_PERFU_Wrapper(pl.LightningModule):
         inp = torch.randn([1, res*res, input_dim+2])
         flops = FlopCountAnalysis(self.supert, inp)
         kwargs['flops'] = flops.total()
+
+        # print(kwargs['parameters'], kwargs['flops'])
+        # assert(0)
         self.mixup = MixupSaliency(
             cutmix_alpha=1.0, cutmix_minmax=None,
             prob=1.0,  mode='batch',
