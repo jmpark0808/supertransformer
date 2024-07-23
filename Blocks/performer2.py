05bd69a32e81ae679095ae602d2ef1df27d66f3c
Jinman <jmpark0808@gmail.com>
2024-07-23 09:48:39 -0400

    Try doing mean instead of cls token for performer

--- Blocks/performer2.py
+++ Blocks/performer2.py
@@ -399,7 +399,7 @@ class ViP(nn.Module):
         )
 
         # self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 4, dim))
+        # self.cls_token = nn.Parameter(torch.randn(1, 4, dim))
         self.dropout = nn.Dropout(emb_dropout)
         self.locations = nn.Sequential(nn.Linear(2, dim), nn.LayerNorm(dim))
 
@@ -409,8 +409,8 @@ class ViP(nn.Module):
         self.to_latent = nn.Identity()
 
         self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim*4),
-            nn.Linear(dim*4, num_classes)
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
         )
 
 
@@ -428,16 +428,16 @@ class ViP(nn.Module):
         x = self.to_patch_embedding(x)
         b, n, _ = x.shape
 
-        cls_tokens = repeat(self.cls_token, '1 c d -> b c d', b = b)
+        # cls_tokens = repeat(self.cls_token, '1 c d -> b c d', b = b)
 
         x += locations
-        x = torch.cat((cls_tokens, x), dim=1)
+        # x = torch.cat((cls_tokens, x), dim=1)
         # x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)
 
         x = self.transformer(x)
 
-        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, :4]
+        x = x.mean(dim = 1) # if self.pool == 'mean' else x[:, :4]
 
         x = x.reshape(x.size(0), -1)
         x = self.to_latent(x)
