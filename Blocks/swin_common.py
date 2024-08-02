b877f71644db7303fe0eaeed12df84ff787d296a
Jinman <jmpark0808@gmail.com>
2024-08-02 13:29:10 -0400

    Try PerfU similar to swinu

--- Blocks/swin_common.py
+++ Blocks/swin_common.py
@@ -725,6 +725,33 @@ class PatchExpand(nn.Module):
         x= self.norm(x)
 
         return x
+    
+
+class PatchExpandLowerDim(nn.Module):
+    def __init__(self, input_resolution, dim, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.dim = dim
+        self.expand = nn.Linear(dim, 2*dim, bias=False)
+        self.norm = norm_layer(dim)
+
+    def forward(self, x):
+        """
+        x: B, H*W, C
+        """
+
+        H, W = self.input_resolution
+        x = self.expand(x) # 通道扩大4倍
+
+        B, L, C = x.shape
+        assert L == H * W, "input feature has wrong size"
+
+        x = x.view(B, H, W, C)
+        x = rearrange(x, 'b h w (p1 p2 c)-> b (h p1) (w p2) c', p1=2, p2=2, c=C//4) # 尺寸扩大2倍
+        x = x.view(B,-1,C//4)
+        x= self.norm(x)
+
+        return x
 
 class BasicLayer_up(nn.Module):
     """ A basic Swin Transformer layer for one stage.
