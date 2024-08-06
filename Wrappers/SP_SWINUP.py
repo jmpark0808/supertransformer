07afba87b1e64bf47673dbe1c622e50f04f58ad8
Jinman <jmpark0808@gmail.com>
2024-08-06 15:11:55 -0400

    Remove flops counter

--- Wrappers/SP_SWINUP.py
+++ Wrappers/SP_SWINUP.py
@@ -47,8 +47,8 @@ class SP_SWINUP_Wrapper(pl.LightningModule):
         inp = torch.randn([1, input_dim+2, res, res])
         flops = FlopCountAnalysis(self.supert, inp)
         kwargs['flops'] = flops.total()
-        print(kwargs['parameters'] , kwargs['flops'])
-        assert(0)
+        # print(kwargs['parameters'] , kwargs['flops'])
+        # assert(0)
         self.mixup = MixupSaliency(
             cutmix_alpha=1.0, cutmix_minmax=None,
             prob=1.0,  mode='batch',
