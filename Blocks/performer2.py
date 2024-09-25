ccd867b03c04d22cd3e0998b1c51b5ba9bd83789
Jinman Park <jmpark0808@gmail.com>
2024-09-25 10:28:21 -0400

    Get ready for imgnet pretrain

--- Blocks/performer2.py
+++ Blocks/performer2.py
@@ -574,11 +574,6 @@ class TransformerDec(nn.Module):
         attn_out_bias = True
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                PreNorm(dim, SelfAttention(dim, causal = causal, heads = heads, dim_head = dim_head, local_heads = local_attn_heads,
-                                            local_window_size = local_window_size, nb_features = nb_features,
-                                              generalized_attention = generalized_attention, kernel_fn = kernel_fn,
-                                                dropout = attn_dropout, no_projection = no_projection, qkv_bias = qkv_bias,
-                                                  attn_out_bias = attn_out_bias)),
                 PreNorm(dim, CrossAttention(dim, causal = causal, heads = heads, dim_head = dim_head, local_heads = local_attn_heads,
                                              local_window_size = local_window_size, nb_features = nb_features,
                                                generalized_attention = generalized_attention, kernel_fn = kernel_fn,
@@ -589,11 +584,10 @@ class TransformerDec(nn.Module):
 
         
     def forward(self, x, context):
-        for attn, cross_attn, ff in self.layers:
-            x = attn(x) + x
-            x = cross_attn(x, context=context) + x
-            x = ff(x) + x
-        return x
+        for attn, ff in self.layers:
+            context = attn(x, context=context) + context
+            context = ff(context) + context
+        return context
 
 
 class ViP(nn.Module):
@@ -641,7 +635,7 @@ class ViP(nn.Module):
         if self.task == 'sod':
             num_classes = 1
             self.decoder = nn.ModuleList()
-            for i in range(depth):
+            for i in range(depth-1):
                 self.decoder.append(TransformerDec(dim, 1, heads, dim_head, mlp_dim, emb_dropout, dropout))
             
             self.mlp_head = nn.Sequential(
@@ -651,11 +645,11 @@ class ViP(nn.Module):
         else:
             num_classes = 1000 
             self.create_patches = nn.Sequential(
-            Rearrange('b (h p1 w p2) c -> b (h w) (p1 p2 c)', h = image_size, p1 = 4, p2 = 2),
+            # Rearrange('b (h p1 w p2) c -> b (h w) (p1 p2 c)', h = image_size, p1 = 4, p2 = 4),
             # # nn.LayerNorm(patch_dim),
             # nn.Linear(dim*16, 256),
-            # WindowSampling(dim, 16, 16, 4, 1.0, dropout, emb_dropout),
-            TFM(256, 2, 16, 16, 256*4, emb_dropout, dropout)
+            WindowSampling(dim, 16, 16, 4, 1.0, dropout, emb_dropout),
+            TFM(256, 1, 16, 16, 256*4, emb_dropout, dropout)
             # nn.LayerNorm(dim)
             )
             self.mlp_head = nn.Sequential(
@@ -696,13 +690,10 @@ class ViP(nn.Module):
         x = self.dropout(x)
         # x = self.ln(x)
 
-        dec = x
-
         fts = []
         for layer in self.transformer:
             x = layer(x)
             fts.append(x)
-        
         
         fts.reverse()
         fts = fts[1:]
@@ -722,9 +713,9 @@ class ViP(nn.Module):
             return self.mlp_head(x)
         else:
             for idx, layer in enumerate(self.decoder):
-                dec = layer(dec,x)
-            dec = self.to_latent(dec)
-            return self.mlp_head(dec)
+                x = layer(fts[idx],x)
+            x= self.to_latent(x)
+            return self.mlp_head(x)
         
         
 
