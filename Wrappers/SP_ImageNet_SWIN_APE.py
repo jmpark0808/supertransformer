9b8b25f8d466c37ff704234d7c3ba60a9e58562b
Jinman Park <jmpark0808@gmail.com>
2024-09-30 23:30:41 -0400

    Use reducelr

--- Wrappers/SP_ImageNet_SWIN_APE.py
+++ Wrappers/SP_ImageNet_SWIN_APE.py
@@ -105,11 +105,11 @@ class SP_ImageNet_OGSWIN_APE_Wrapper(pl.LightningModule):
         
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=0.05)
 
-        self.trainer.fit_loop.setup_data()
-        dataset= self.trainer.train_dataloader
-        self.scheduler = CosineAnnealingWarmRestarts(optimizer, len(dataset)*(self.total_train_epochs-self.warmup_epochs),
-                                                      1, 5e-8)
-        # self.scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, min_lr = 5e-6)
+        # self.trainer.fit_loop.setup_data()
+        # dataset= self.trainer.train_dataloader
+        # self.scheduler = CosineAnnealingWarmRestarts(optimizer, len(dataset)*(self.total_train_epochs-self.warmup_epochs),
+        #                                               1, 5e-8)
+        self.scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=7, min_lr = 5e-8)
         
         return optimizer
     
@@ -192,13 +192,15 @@ class SP_ImageNet_OGSWIN_APE_Wrapper(pl.LightningModule):
 
         self.log('loss', loss.item(), sync_dist=True)
         self.iteration += 1
-        if self.current_epoch >= self.warmup_epochs:
-            self.scheduler.step()
+        # if self.current_epoch >= self.warmup_epochs:
+        #     self.scheduler.step()
         return loss
 
     def on_validation_epoch_end(self):
         acc = self.val_acc/self.val_num_samples
         self.log('Validation Accuracy', acc, sync_dist=True)
+        if self.current_epoch >= self.warmup_epochs:
+            self.scheduler.step(acc)
         self.validation_step_outputs.clear()
 
     def on_validation_start(self):
