fdec6ee331ec0fbf7cf30e71c945ab7d150461a5
Jinman Park <jmpark0808@gmail.com>
2024-10-04 18:16:29 -0400

    Change swinum to cosine as well

--- Wrappers/SP_SWINUM.py
+++ Wrappers/SP_SWINUM.py
@@ -117,18 +117,18 @@ class SP_SWINUM_Wrapper(pl.LightningModule):
         parameters = [{'params': has_decay},
                 {'params': no_decay, 'weight_decay': 0.}]
         optimizer = torch.optim.AdamW(parameters, lr=self.lr, weight_decay=0.05)
-        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            mode='min',
-            factor=0.1,
-            patience=self.es_patience//2,
-            min_lr=5e-8,
-            verbose=True)
+        # self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        #     optimizer,
+        #     mode='min',
+        #     factor=0.1,
+        #     patience=self.es_patience//2,
+        #     min_lr=5e-8,
+        #     verbose=True)
         
-        # self.trainer.fit_loop.setup_data()
-        # dataset= self.trainer.train_dataloader
-        # self.scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, len(dataset)*(self.total_train_epochs-self.warmup_epochs),
-        #                                               1, 5e-8)
+        self.trainer.fit_loop.setup_data()
+        dataset= self.trainer.train_dataloader
+        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, len(dataset)*(self.total_train_epochs-self.warmup_epochs),
+                                                      1, 5e-8)
         return optimizer
       
     def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_closure):
@@ -247,8 +247,8 @@ class SP_SWINUM_Wrapper(pl.LightningModule):
         self.num_samples += features.size(0)
         self.log('loss', loss.item())
         self.iteration += 1
-        # if self.current_epoch >= self.warmup_epochs:
-        #     self.scheduler.step()
+        if self.current_epoch >= self.warmup_epochs:
+            self.scheduler.step()
         return loss
 
     def validation_step(self, batch, batch_idx, dataloader_idx):
@@ -340,8 +340,8 @@ class SP_SWINUM_Wrapper(pl.LightningModule):
         self.log('Test Max F Threshold', thlist[torch.argmax(f_score)])
 
         self.log('Test MAE', self.maes_test/self.mean_num_test)
-        if self.current_epoch >= self.warmup_epochs:
-            self.scheduler.step(torch.mean(torch.stack(self.validation_step_outputs)))
+        # if self.current_epoch >= self.warmup_epochs:
+        #     self.scheduler.step(torch.mean(torch.stack(self.validation_step_outputs)))
         self.validation_step_outputs.clear()
 
     def on_validation_start(self):
