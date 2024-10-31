3c5ec0b7b03ac813cd4a0743b70ed72426afbc04
Jinman Park <jmpark0808@gmail.com>
2024-10-31 13:15:22 -0400

    ROPE without CPE but add FFT

--- Blocks/swinunet_mix_rope.py
+++ Blocks/swinunet_mix_rope.py
@@ -107,7 +107,7 @@ class SwinUTransformer(nn.Module):
 
         self.upsample = nn.Upsample(size=img_size[0])
         self.sod_head = nn.Linear(sum(embed_dim), 1)
-        self.locations = swinencoder.locations
+        # self.locations = swinencoder.locations
 
         
         self.apply(self._init_weights)
@@ -133,12 +133,12 @@ class SwinUTransformer(nn.Module):
     def no_weight_decay_keywords(self):
         return {'relative_position_bias_table'}
 
-    def forward_features(self, x, pos):
+    def forward_features(self, x):
         x = self.patch_embed(x)
  
         x = self.pos_drop(x)
 
-        x = x + pos
+     
         ft = []
         for layer in self.layers:
             ds, x = layer(x)
@@ -167,10 +167,8 @@ class SwinUTransformer(nn.Module):
         lbp = x[:, -10:, :, :]
         color = x[:, 2:8, :, :]
         x = torch.cat((color, fft), dim=1)
-        locations = centroids.permute(0, 2, 3, 1)
-        locations = self.locations(locations)
-        locations = locations.reshape(locations.size(0), -1, locations.size(3))
-        x = self.forward_features(x, locations)
+        
+        x = self.forward_features(x)
         x = self.sod_head(x)
 
         return x
