d49aaecbb43890d8f4854631f4441d9261f29036
Jinman Park <jmpark0808@gmail.com>
2024-10-30 23:39:53 -0400

    ROPE with FFT

--- Analysis/visualizations/scatterplot_flops_params.py
+++ Analysis/visualizations/scatterplot_flops_params.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from adjustText import adjust_text
-
+import matplotlib
 
 data = {'SAMNet': {'Params': 1.33, 'FLOPs': 0.5, 'MAE': 0.058, 'F1': 0.835, 'Marker': 'x', 'colour': 'r'},
         'HVPNet': {'Params': 1.23, 'FLOPs': 1.1, 'MAE': 0.058, 'F1': 0.839, 'Marker': 'x', 'colour': 'r'},
         'CorrNet': {'Params': 4.09, 'FLOPs': 21.1, 'MAE': 0.0466, 'F1': 0.847, 'Marker': 'x', 'colour': 'r'},
         'SeaNet': {'Params': 2.76, 'FLOPs': 1.7, 'MAE': 0.045, 'F1': 0.854, 'Marker': 'x', 'colour': 'r'},
         'MEANet': {'Params': 3.27, 'FLOPs': 9.62, 'MAE': 0.0454, 'F1': 0.863, 'Marker': 'x', 'colour': 'r'},
-        'MSHNet': {'Params': 4.07, 'FLOPs': 6.11, 'MAE': 0.1251, 'F1': 0.7046, 'Marker': 'x', 'colour': 'r'},
+        'MSHNet': {'Params': 4.07, 'FLOPs': 6.11, 'MAE': 0.1251, 'F1': 0.7046+0.12, 'Marker': 'x', 'colour': 'r'},
         'Ours (XS)': {'Params': 1.12, 'FLOPs': 0.46, 'MAE': 0.053, 'F1': 0.847, 'Marker': 'o', 'colour': 'g'},
          'Ours (S)': {'Params': 2.18, 'FLOPs': 0.87, 'MAE': 0.045, 'F1': 0.866, 'Marker': 'o', 'colour': 'g'} }
 
@@ -19,10 +19,12 @@ fig, ax = plt.subplots(2, 2, figsize=(10, 10))
 label_fontsize = 15
 ax[0, 0].set_xlabel('Model Params. (M)', fontsize=label_fontsize)
 ax[0, 0].set_ylabel('F1-score', fontsize=label_fontsize)
-
+ax[0, 0].set_yticks(np.linspace(0.82, 0.90, 8))
+ax[0, 0].set_yticklabels(["0.70","0.72","...","0.82","0.84","0.86","0.88","0.90"]) 
 ax[0, 1].set_xlabel('GFLOPs', fontsize=label_fontsize)
 ax[0, 1].set_ylabel('F1-score', fontsize=label_fontsize)
-
+ax[0, 1].set_yticks(np.linspace(0.82, 0.90, 8))
+ax[0, 1].set_yticklabels(["0.70","0.72","...","0.82","0.84","0.86","0.88","0.90"]) 
 ax[1, 0].set_xlabel('Model Params. (M)', fontsize=label_fontsize)
 ax[1, 0].set_ylabel('MAE', fontsize=label_fontsize)
 
@@ -30,6 +32,8 @@ ax[1, 1].set_xlabel('GFLOPs', fontsize=label_fontsize)
 ax[1, 1].set_ylabel('MAE', fontsize=label_fontsize)
 ax[0,1].set_xscale('log')
 ax[1,1].set_xscale('log')
+ax[0,1].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax[1,1].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 text1 = []
 text2 = []
 text3 = []
@@ -54,10 +58,10 @@ ax[0,1].plot([0.46, 0.87], [0.847, 0.866], color='g')
 ax[1,0].plot([1.12, 2.18], [0.053, 0.045], color='g')
 ax[1,1].plot([0.46, 0.87], [0.053, 0.045], color='g')
 
-adjust_text(text1,  arrowprops=dict(arrowstyle="-", color='b', lw=1), ax=ax[0,0], min_arrow_len=5)
-adjust_text(text2,  arrowprops=dict(arrowstyle="-", color='b', lw=1), ax=ax[1,0], min_arrow_len=5)
-adjust_text(text3,  arrowprops=dict(arrowstyle="-", color='b', lw=1), ax=ax[0,1], min_arrow_len=5)
-adjust_text(text4,  arrowprops=dict(arrowstyle="-", color='b', lw=1), ax=ax[1,1], min_arrow_len=5)
+adjust_text(text1,   ax=ax[0,0], min_arrow_len=5) #arrowprops=dict(arrowstyle="-", color='b', lw=1),
+adjust_text(text2,    ax=ax[1,0], min_arrow_len=5)
+adjust_text(text3,   ax=ax[0,1], min_arrow_len=5)
+adjust_text(text4,   ax=ax[1,1], min_arrow_len=5)
 
 
 plt.show()
