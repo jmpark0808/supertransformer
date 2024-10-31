dff9659a3653bfb518a941f2133c3992ad24bc8a
Jinman Park <jmpark0808@gmail.com>
2024-10-30 23:39:53 -0400

    ROPE with FFT

--- Blocks/swinunet_mix_rope.py
+++ Blocks/swinunet_mix_rope.py
@@ -53,7 +53,7 @@ class SwinUTransformer(nn.Module):
         super().__init__()
 
 
-        swinencoder = SwinTransformer(img_size=img_size, patch_size=patch_size, in_chans=6, num_classes=num_classes,
+        swinencoder = SwinTransformer(img_size=img_size, patch_size=patch_size, in_chans=in_chans-10, num_classes=num_classes,
                                       embed_dim=embed_dim, depths=depths, num_heads=num_heads,
                                       window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                                       drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, drop_path_rate=drop_path_rate,
@@ -163,8 +163,10 @@ class SwinUTransformer(nn.Module):
 
     def forward(self, x):
         centroids = x[:, :2, :, :]
+        fft = x[:, 8:-10, :, :]
+        lbp = x[:, -10:, :, :]
         color = x[:, 2:8, :, :]
-        x = color
+        x = torch.cat((color, fft), dim=1)
         locations = centroids.permute(0, 2, 3, 1)
         locations = self.locations(locations)
         locations = locations.reshape(locations.size(0), -1, locations.size(3))
