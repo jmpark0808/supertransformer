a84d8b2deff49a4284cd7efbe52635058ecf0d60
Ashley Chen <ashleyc@beluga2.int.ets1.calculquebec.ca>
2024-11-06 23:20:09 -0500

    H1-R0-C0-M0

--- dataset/superpixel_fast.py
+++ dataset/superpixel_fast.py
@@ -357,7 +357,7 @@ class SPDatasetExport(data.Dataset):
 
 class SPDataset(data.Dataset):
     def __init__(self, image_list, mask_list, num_seg, size, 
-                  dataloader, data_augmentation=False, coeff=None):
+                  dataloader, data_augmentation=True, coeff=None):
         self.image_list = image_list
         self.mask_list = mask_list
         self.resize_mask = ResizeMask(size)
@@ -398,24 +398,24 @@ class SPDataset(data.Dataset):
         
         if self.data_augmentation:
             features, seq_mask = horizontal_flip(features, self.coeff, 0.5, self.size, (int(self.num_seg**0.5), int(self.num_seg**0.5)), seq_mask)
-            features = rotate(features, self.coeff, 15, 0.5, (self.size, self.size))
+            # features = rotate(features, self.coeff, 15, 0.5, (self.size, self.size))
             
 
         features = torch.tensor(features).float()
-        if self.data_augmentation:
-            randaug = RandAugment(5)
-            res = int(self.num_seg**0.5)
-            color_space = features[:, 2:5].reshape(res, res, 3).permute(2, 0, 1)
-            if np.random.random() < 0.5:
-                color_space = (color_space*255).to(torch.uint8)
-                color_space, _ = randaug(color_space)
-                color_space = color_space.float()
-                color_space /= 255.
-            # plt.imshow(color_space.permute(1, 2, 0).detach().cpu().numpy())
-            # plt.show()
-            color_space = color_space.reshape(3, self.num_seg).permute(1, 0)
+        # if self.data_augmentation:
+        #     randaug = RandAugment(5)
+        #     res = int(self.num_seg**0.5)
+        #     color_space = features[:, 2:5].reshape(res, res, 3).permute(2, 0, 1)
+        #     if np.random.random() < 0.5:
+        #         color_space = (color_space*255).to(torch.uint8)
+        #         color_space, _ = randaug(color_space)
+        #         color_space = color_space.float()
+        #         color_space /= 255.
+        #     # plt.imshow(color_space.permute(1, 2, 0).detach().cpu().numpy())
+        #     # plt.show()
+        #     color_space = color_space.reshape(3, self.num_seg).permute(1, 0)
             
-            features[:, 2:5] = color_space
+        #     features[:, 2:5] = color_space
     
         return {'features': features, 'seq_mask': torch.tensor(seq_mask),
                  'segments': torch.tensor(segments), 'mask': mask, 
