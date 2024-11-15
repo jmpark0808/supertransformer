9d89a3e61577c58f7c462ccf548a8c5864a6b497
Jinman Park <jmpark0808@gmail.com>
2024-11-14 21:16:40 -0500

    Use all features for CROPE

--- Blocks/swin_encoder_crope.py
+++ Blocks/swin_encoder_crope.py
@@ -116,7 +116,7 @@ class SwinTransformer(nn.Module):
 
     def forward_features(self, x):
         centroids = x[:, :2, :, : ]
-        x = x[:, 2:-10, :, :]
+        x = x[:, 2:, :, :]
 
         x = self.patch_embed(x)
  
