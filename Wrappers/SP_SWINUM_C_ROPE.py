Fix test step for rope and crope, add centroid masking for zeros for imgnet crope

Wrappers/SP_SWINUM_C_ROPE.py
@@ -391,6 +391,7 @@ class SP_SWINUM_C_ROPE_Wrapper(pl.LightningModule):
         seq_mask = batch['seq_mask']
         segments = batch['segments']
         mask = batch['mask']
+        names = batch['file_name']
 
 
         # forward pass
@@ -398,10 +399,10 @@ class SP_SWINUM_C_ROPE_Wrapper(pl.LightningModule):
         features = features.reshape(features.size(0), res, res, -1).permute(0, 3, 1, 2)
         pred = self.forward(features)
 
-        pred_numpy = torch.sigmoid(pred).detach().cpu().numpy() # batch, seq_len, 1
-        seq_mask_numpy = seq_mask.detach().cpu().numpy()
+        pred_numpy = torch.sigmoid(pred).detach().cpu() # batch, seq_len, 1
+    
         batch_size = mask.shape[0]
-        img_size = mask.shape[2]
+        img_size = self.size
         if torch.sum(segments) != 0 :
 
             segments = segments.reshape([batch_size, -1]) # batch, img_size^2
@@ -409,9 +410,10 @@ class SP_SWINUM_C_ROPE_Wrapper(pl.LightningModule):
             samples = []
             for masked, labels in zip(pred_numpy, segments.cpu().numpy()):
                 plt_image = masked[labels-1].reshape([img_size, img_size])
+                plt_image = F.interpolate(plt_image.unsqueeze(0).unsqueeze(0), (mask.size(2), mask.size(3)), mode='bilinear')
                 samples.append(plt_image)
 
-            samples = torch.tensor(np.expand_dims(np.array(samples), 1)).cuda()
+            samples = torch.cat(samples, dim=0).cuda()
         else:
             samples = torch.sigmoid(pred).reshape(pred.size(0), 1, res, res)
             samples = F.interpolate(samples, (self.image_size, self.image_size), mode='bilinear').detach().cpu()
@@ -419,8 +421,14 @@ class SP_SWINUM_C_ROPE_Wrapper(pl.LightningModule):
 
         # tensorboard.add_images('Test GT', samples_mask, self.test_iteration)
         # tensorboard.add_images('Test Image', img, self.test_iteration)
+        # for sample, name in zip(samples, names):
+        #     name = name.split('/')[-1]
+        #     sample = ((sample > 0.5).float().permute(1, 2, 0).detach().cpu().numpy()*255).astype(np.uint8)
+            
+ 
+        #     cv2.imwrite('/home/eddie/Qualitative/SF/DUTS-TE/'+name, sample)
 
-        mae = torch.sum(torch.mean(torch.abs(samples - mask), dim=tuple(range(1, len(samples.size())))))
+        mae = torch.sum(torch.mean(torch.abs((samples >= 0.5).float() - mask), dim=tuple(range(1, len(samples.size())))))
         self.maes += mae
         self.mean_num += features.size(0)
 