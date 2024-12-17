8eb7ba35df93f0f2714c70a3a5f17971df86a4bd
Jinman Park <jmpark0808@gmail.com>
2024-12-17 15:11:01 -0500

    Remove the 48 dim filter for rope and crope

--- Blocks/swin_rope.py
+++ Blocks/swin_rope.py
@@ -104,17 +104,17 @@ class WindowAttentionRoPE(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
         
-        if dim < 48:
-            t_x, t_y = init_t_xy(end_x=self.window_size[1], end_y=self.window_size[0])
-            self.register_buffer('rope_t_x', t_x)
-            self.register_buffer('rope_t_y', t_y)
+        # if dim < 48:
+        t_x, t_y = init_t_xy(end_x=self.window_size[1], end_y=self.window_size[0])
+        self.register_buffer('rope_t_x', t_x)
+        self.register_buffer('rope_t_y', t_y)
 
-            freqs = init_random_2d_freqs(
-                head_dim=self.dim // self.num_heads, num_heads=self.num_heads, theta=rope_theta, 
-                rotate=True
-            )
-            
-            self.rope_freqs = nn.Parameter(freqs, requires_grad=True)
+        freqs = init_random_2d_freqs(
+            head_dim=self.dim // self.num_heads, num_heads=self.num_heads, theta=rope_theta, 
+            rotate=True
+        )
+        
+        self.rope_freqs = nn.Parameter(freqs, requires_grad=True)
 
     def forward(self, x,  mask=None):
         """
@@ -130,21 +130,21 @@ class WindowAttentionRoPE(nn.Module):
 
         q = q * self.scale
 
-        if self.dim < 48:
+        # if self.dim < 48:
 
-            freqs_cis = compute_cis(self.rope_freqs, self.rope_t_x, self.rope_t_y)
+        freqs_cis = compute_cis(self.rope_freqs, self.rope_t_x, self.rope_t_y)
 
-            # if not self.training:
-                
-                
-            #     import matplotlib.pyplot as plt
-            #     import numpy as np
-            #     plt.scatter(self.rope_t_x.detach().cpu().numpy(), self.rope_t_y.detach().cpu().numpy())
-            #     for x, y, t in zip(self.rope_t_x.detach().cpu().numpy(), self.rope_t_y.detach().cpu().numpy(), [str(o) for o in range(len(np.squeeze(self.rope_t_x.detach().cpu().numpy())))]):
-            #         plt.text(x, y, t)
-            #     plt.title(f'{self.dim}')
-            #     plt.show()
-            q, k = apply_rotary_emb(q, k, freqs_cis)
+        # if not self.training:
+            
+            
+        #     import matplotlib.pyplot as plt
+        #     import numpy as np
+        #     plt.scatter(self.rope_t_x.detach().cpu().numpy(), self.rope_t_y.detach().cpu().numpy())
+        #     for x, y, t in zip(self.rope_t_x.detach().cpu().numpy(), self.rope_t_y.detach().cpu().numpy(), [str(o) for o in range(len(np.squeeze(self.rope_t_x.detach().cpu().numpy())))]):
+        #         plt.text(x, y, t)
+        #     plt.title(f'{self.dim}')
+        #     plt.show()
+        q, k = apply_rotary_emb(q, k, freqs_cis)
 
 
         attn = (q @ k.transpose(-2, -1))
