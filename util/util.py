361f84dec65da4a0125cdedb092fb8e0852f32d6
Jinman Park <jmpark0808@gmail.com>
2025-02-17 13:01:04 -0500

    Configure input dim for moments

--- util/util.py
+++ util/util.py
@@ -151,6 +151,7 @@ def resample_2d(points, N):
 
 def get_input_dim(args):
     d = args.get('dataloader')
+    moments = args.get('moments')
     if d == 'SP' or d == 'SPLAP' or d == 'INPE':
         return 3
     elif d == 'SPF' or d == 'DUTS':
@@ -159,10 +160,13 @@ def get_input_dim(args):
                 'YD', 'SPGSWIN', 'YDG',
               'ImageNet_SWIN',  'SpeedLimits', 
                 'SPSpeedLimits',  'SPFRS', 'SPRS', 'SPFFT' ]:
-        if args.get('ignore_phase'):
-            return 6+(args.get('coeff'))+10
+        if moments:
+            return 6+8+10
         else:
-            return 6+(args.get('coeff')*2)+10
+            if args.get('ignore_phase'):
+                return 6+(args.get('coeff'))+10
+            else:
+                return 6+(args.get('coeff')*2)+10
     elif d== 'SPGIFFT' or d == 'ImageNet_PyG':
         if args.get('ignore_phase'):
             return 6+(args.get('coeff'))
