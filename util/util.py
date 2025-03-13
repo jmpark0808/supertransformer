c10efb45ec92bfcff829fee17a99ea95728a6128
Jinman Park <jmpark0808@gmail.com>
2025-03-13 10:47:26 -0400

    Bring back moments

--- util/util.py
+++ util/util.py
@@ -163,7 +163,7 @@ def get_input_dim(args):
         if args.get('ignore_phase'):
             return 6+(args.get('coeff'))+10
         else:
-            return 6+(args.get('coeff'))+10
+            return 6+(args.get('coeff'))+8+10
     elif d== 'SPGIFFT' or d == 'ImageNet_PyG':
         if args.get('ignore_phase'):
             return 6+(args.get('coeff'))
