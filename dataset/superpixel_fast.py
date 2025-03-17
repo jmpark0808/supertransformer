3cfcd1402b25b71c26d08987ebb6f8bb4870c107
Jinman Park <jmpark0808@gmail.com>
2025-03-17 13:35:06 -0400

    Put phase back in

--- dataset/superpixel_fast.py
+++ dataset/superpixel_fast.py
@@ -480,18 +480,17 @@ class SPDataset(data.Dataset):
         colour_and_centroid = features[:, :8]
         lbp = features[:, -10:]
         features_amp = np.concatenate((features_amp[:, :front], features_amp[:, -back:]), axis=1)
+        features_phase = np.concatenate((features_phase[:, :front], features_phase[:, -back:]), axis=1)
         
-        
-        # plt.bar(np.arange(take*2),np.concatenate((amp_front, amp_back), axis=1)[0])
-        # plt.show()
-         
+        # plt.plot(features_phase[0])
+        # plt.show()         
         if self.data_augmentation:
             moments = rotate_moments(moments, 0.5, 15)
             moments = log_moments(moments)
 
             
-            centroids, colour, features_amp, moments, lbp, seq_mask = horizontal_flip_moments(colour_and_centroid[:, :2], colour_and_centroid[:, 2:],
-                                                features_amp, moments, lbp, 0.5, self.size, (int(self.num_seg**0.5), int(self.num_seg**0.5)), seq_mask)
+            centroids, colour, features_amp, features_phase, moments, lbp, seq_mask = horizontal_flip_moments(colour_and_centroid[:, :2], colour_and_centroid[:, 2:],
+                                                features_amp, features_phase, moments, lbp, 0.5, self.size, (int(self.num_seg**0.5), int(self.num_seg**0.5)), seq_mask)
             
             colour_and_centroid = np.concatenate((centroids, colour), 1)
             # features = rotate(features, self.coeff, 15, 0.5, (self.size, self.size))
@@ -500,7 +499,7 @@ class SPDataset(data.Dataset):
         
        
         if self.coeff != 0:
-            features_np = np.concatenate((colour_and_centroid, features_amp, lbp), 1)
+            features_np = np.concatenate((colour_and_centroid, features_amp, features_phase, lbp), 1)
         else:
             features_np = np.concatenate((colour_and_centroid, lbp), 1)
         features = torch.tensor(features_np).float()
