dc1bdea5df3d4c718b436fe96557f8a84ba1fc1b
Jinman Park <jmpark0808@gmail.com>
2025-05-13 15:57:17 -0400

    Forgot SINU

--- Blocks/swinunet_mix_sinu.py
+++ Blocks/swinunet_mix_sinu.py
@@ -75,7 +75,7 @@ class SwinUTransformer(nn.Module):
         super().__init__()
 
 
-        swinencoder = SwinTransformer(img_size=img_size, patch_size=patch_size, in_chans=in_chans, num_classes=num_classes,
+        swinencoder = SwinTransformer(img_size=img_size, patch_size=patch_size, in_chans=16, num_classes=num_classes,
                                       embed_dim=embed_dim, depths=depths, num_heads=num_heads,
                                       window_size=window_size, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                                       drop_rate=drop_rate, attn_drop_rate=attn_drop_rate, drop_path_rate=drop_path_rate,
@@ -193,9 +193,12 @@ class SwinUTransformer(nn.Module):
 
 
     def forward(self, x):
-        features = x[:, 2:, :, :]
+        color = x[:, 2:8, :, :]
+        lbp = x[:, -10:, :, :]
 
-        x = self.forward_features(features)
+        x = torch.cat((color, lbp), dim=1)
+
+        x = self.forward_features(x)
         x = self.sod_head(x)
 
         return x
